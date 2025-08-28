042c2a94ea70a1b456fd5b607d270ecdef11c417
nyobee <nyobeentanglaud@gmail.com>
2025-08-28 09:36:02 +0700

    remove property setter fixtures

--- erpnext_initial_base/customizations/fixtures_list.py
+++ erpnext_initial_base/customizations/fixtures_list.py
@@ -1,6 +1,6 @@
 from .fixtures.client_script import client_script_fixtures
 from .fixtures.custom_docperm import custom_docperm_fixtures
-from .fixtures.property_setter import property_setter_fixtures
+# from .fixtures.property_setter import property_setter_fixtures
 from .fixtures.role import role_fixtures
 from .fixtures.role_profile import role_profile_fixtures
 
@@ -9,5 +9,5 @@ from .fixtures.role_profile import role_profile_fixtures
 fixtures_list = role_fixtures + \
     role_profile_fixtures + \
     custom_docperm_fixtures + \
-    client_script_fixtures + \
-    property_setter_fixtures
+    client_script_fixtures
+    # property_setter_fixtures
